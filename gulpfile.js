49a34f3ab143c908e202b53afd7563fbdaa9df92
ThiagoPerdigao <thiagovitor299@gmail.com>
2025-09-21 14:11:09 -0300

    Fix SVG

--- gulpfile.js
+++ gulpfile.js
@@ -7,15 +7,12 @@ function copyIcons() {
 	const nodeSource = path.resolve('nodes', '**', '*.{png,svg}');
 	const nodeDestination = path.resolve('dist', 'nodes');
 
-	src(nodeSource).pipe(dest(nodeDestination));
+	// Copia ícones de nodes (sempre deve existir)
+	src(nodeSource, { allowEmpty: true }).pipe(dest(nodeDestination));
 
 	const credSource = path.resolve('credentials', '**', '*.{png,svg}');
 	const credDestination = path.resolve('dist', 'credentials');
 
-	// Só copia se a pasta existir
-	try {
-		return src(credSource).pipe(dest(credDestination));
-	} catch (e) {
-		return Promise.resolve(); // não quebra o build
-	}
+	// Copia ícones de credenciais (só se a pasta existir, não quebra se não tiver)
+	return src(credSource, { allowEmpty: true }).pipe(dest(credDestination));
 	}
